6be3fcb6c2942fd9cc0d37e47c4cfa6b91e3ef97
somuncu19@itu.edu.tr <somuncu19@itu.edu.tr>
2025-04-09 15:07:28 +0300

    update RccRegisterDef_t

--- stm32f410rb/driver/rcc.h
+++ stm32f410rb/driver/rcc.h
@@ -8,29 +8,29 @@ typedef struct
     volatile uint32_t RCC_CFGR;
     volatile uint32_t RCC_CIR;
     volatile uint32_t RCC_AHB1RSTR;
-    volatile uint32_t RCC_RESERVED[2];
+    volatile uint32_t RCC_RESERVED0[3];
     volatile uint32_t RCC_APB1RSTR;
     volatile uint32_t RCC_APB2RSTR;
-    volatile uint32_t RCC_RESERVED;
+    volatile uint32_t RCC_RESERVED1[2];
     volatile uint32_t RCC_AHB1ENR;
-    volatile uint32_t RCC_RESERVED[2];
+    volatile uint32_t RCC_RESERVED2[3];
     volatile uint32_t RCC_APB1ENR;
     volatile uint32_t RCC_APB2ENR;
-    volatile uint32_t RESERVED;
+    volatile uint32_t RESERVED3[2];
     volatile uint32_t RCC_AHB1LPENR;
-    volatile uint32_t RCC_RESERVED[2];
+    volatile uint32_t RCC_RESERVED4[3];
     volatile uint32_t RCC_APB1LPENR;
     volatile uint32_t RCC_APB2LPENR;
-    volatile uint32_t RESERVED;
+    volatile uint32_t RESERVED5[2];
     volatile uint32_t RCC_BDCR;
     volatile uint32_t RCC_CSR;
-    volatile uint32_t RCC_RESERVED;
+    volatile uint32_t RCC_RESERVED6[2];
     volatile uint32_t RCC_SSCGR;
-    volatile uint32_t RESERVED;
+    volatile uint32_t RESERVED7[2];
     volatile uint32_t RCC_DCKCFGR;
-    volatile uint32_t RCC_RESERVED;
+    volatile uint32_t RCC_RESERVED8;
     volatile uint32_t RCC_DCKCFGR2;
-}RccRegisterMap_t;
+}RccRegisterDef_t;
 
 
 
