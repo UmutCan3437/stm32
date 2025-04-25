500c5f481af1ee69367758b314077c31ab2b6ca3
Umut Can Somuncu <umutcan@Umut-MacBook-Air.local>
2025-04-25 22:19:08 +0300

    create base bsp

---
+++ bsp/stm32f4xx.h
@@ -0,0 +1,18 @@
+
+/*TODO: add comment */
+#define APB1_BUS_BASE_ADDR  (0x40000000U)
+#define APB2_BUS_BASE_ADDR  (0x40010000U)
+#define AHB1_BUS_BASE_ADDR  (0x40020000U)
+
+
+/*GPIO Peripherals Base Addresses*/
+
+#define GPIOA_BASE_ADDR            (AHB1_BUS_BASE_ADDR)
+#define GPIOB_BASE_ADDR            (AHB1_BUS_BASE_ADDR + 0x400U)
+#define GPIOC_BASE_ADDR            (AHB1_BUS_BASE_ADDR + 0x800U)
+
+
+/*RCC Base Address*/
+#define RCC_BASE_ADDR              (AHB1_BUS_BASE_ADDR + 3800U)
+
+
